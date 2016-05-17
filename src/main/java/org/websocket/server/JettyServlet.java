5f32eef66ba7c7579ed2cac814bb49a803a7810d
VyacheslavRarata <cayneir@gmail.com>
2016-05-17 20:48:45 +0300

    added servlet for server

---
+++ src/main/java/org/websocket/server/JettyServlet.java
@@ -0,0 +1,18 @@
+package org.websocket.server;
+
+import javax.servlet.annotation.WebServlet;
+
+import org.eclipse.jetty.websocket.servlet.WebSocketServlet;
+import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
+
+@WebServlet("/jetty")
+public class JettyServlet extends WebSocketServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void configure(WebSocketServletFactory factory) {
+		factory.register(JettyServlet.class);
+	}
+
+}
